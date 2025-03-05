fd63573d4593fe3ded0148227ff53851589895ac
PigeonPC <olgakatinka@gmail.com>
2025-03-05 11:15:51 +0100

    Commit2

--- src/LoanItems.java
+++ src/LoanItems.java
@@ -1,2 +1,11 @@
 public class LoanItems {
+    int ID;
+    String type;
+    String title;
+
+    public LoanItems(int ID, String type, String title){
+        this.ID = ID;
+        this.type = type;
+        this.title = title;
+    }
 }
